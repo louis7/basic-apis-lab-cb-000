d3408e289e57d98de561d1854d914bb030475335
louis baptiste <louisvis@ymail.com>
2020-03-13 21:09:58 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,14 @@ class RepositoriesController < ApplicationController
   def search
 
   end
-
   def github_search
-  end
+    client_id = "REPLACE ME"
+    secret = "REPLACE ME"
+
+    resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+      req.params["q"] = params[:query]
+  
+    end
+
+
 end
